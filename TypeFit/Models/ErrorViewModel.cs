68a67ff5b0493d7fe8646bce5f1a1915db1e7b85
David Alexandre <david.paula.dias@everis.com>
2019-10-19 22:41:49 -0300

    Add project files.

---
+++ TypeFit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TypeFit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
