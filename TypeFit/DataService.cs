98267c81a4b7a34ecc4f9f15ed08285c3a8eeaa9
David Alexandre <david.paula.dias@everis.com>
2019-10-20 03:34:06 -0300

    Repositorios

--- TypeFit/DataService.cs
+++ TypeFit/DataService.cs
@@ -1,19 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using TypeFit.Repositories;
 
 namespace TypeFit
 {
     public class DataService : IDataService
     {
         private readonly ApplicationContext context;
+        private readonly IProdutoRepository produtoRepository;
 
-        public DataService(ApplicationContext context)
+        public DataService(ApplicationContext context, IProdutoRepository produtoRepository)
         {
             this.context = context;
+            this.produtoRepository = produtoRepository;
         }
 
         public void InicializaDb()
         {
+            
             context.Database.Migrate();
+            List<ProdutosInicializer> produtos = GetProdutos();
+            produtoRepository.SaveProdutos(produtos);
+        }
+
+        private static List<ProdutosInicializer> GetProdutos()
+        {
+            var json = File.ReadAllText("Produtos.json");
+            var produtos = JsonConvert.DeserializeObject<List<ProdutosInicializer>>(json);
+            return produtos;
         }
     }
 }
---
+++ TypeFit/Repositories/IProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TypeFit.Models;
+using TypeFit.Repositories;
+
+namespace TypeFit
+{
+    public interface IProdutoRepository
+    {
+        Task SaveProdutos(List<ProdutosInicializer> produtos);
+        Task<IList<Produto>> GetProdutos();
+    }
+}
